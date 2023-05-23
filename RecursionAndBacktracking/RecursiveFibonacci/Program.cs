6efc0d96e42cdb4eab1f7948639694db790be06d
Petar Belchev <petar9004@gmail.com>
2023-05-23 07:43:35 +0300

    Added Recursive Fibonacci task solution

---
+++ RecursionAndBacktracking/RecursiveFibonacci/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace RecursiveFibonacci
+{
+	internal class Program
+	{
+		private static int n;
+
+		static void Main(string[] args)
+		{
+			n = int.Parse(Console.ReadLine());
+
+			Console.WriteLine(GetFibonacci(n));
+		}
+
+		private static int GetFibonacci(int n)
+		{
+			if (n <= 1)
+				return 1;
+
+			return GetFibonacci(n - 1) + GetFibonacci(n - 2);
+		}
+	}
+}

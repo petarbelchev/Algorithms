0a3a42b3a0eb2c272ad16757c15b01225a399efe
Petar Belchev <petar9004@gmail.com>
2023-05-25 12:23:12 +0300

    Added Combinatorial Problems solutions

---
+++ CombinatorialProblems/CombinationsWithRepetition/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CombinationsWithRepetition
+{
+	internal class Program
+	{
+		private static string[] elements;
+		private static string[] combination;
+
+		static void Main(string[] args)
+		{
+			elements = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+			combination = new string[int.Parse(Console.ReadLine())];
+
+			Add(0, 0);
+		}
+
+		private static void Add(int index, int startIndex)
+		{
+			if (index >= combination.Length)
+			{
+				Console.WriteLine(string.Join(' ', combination));
+				return;
+			}
+
+			for (int i = startIndex; i < elements.Length; i++)
+			{
+				combination[index] = elements[i];
+				Add(index + 1, i);
+			}
+		}
+	}
+}
---
+++ CombinatorialProblems/NChooseKCount/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace NChooseKCount
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int n = int.Parse(Console.ReadLine());
+			int k = int.Parse(Console.ReadLine());
+
+			int binomCoef = CalcBinomCoef(n, k);
+			Console.WriteLine(binomCoef);
+		}
+
+		private static int CalcBinomCoef(int row, int col)
+		{
+			if (row <= 1 || col == 0 || row == col)
+				return 1;
+
+			return CalcBinomCoef(row - 1, col) + CalcBinomCoef(row - 1, col - 1);
+		}
+	}
+}

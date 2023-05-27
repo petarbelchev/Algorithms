83899f4907b50bd1a6805032bb6d98079a0f9b83
Petar Belchev <petar9004@gmail.com>
2023-05-27 16:21:27 +0300

    Binary Search implementation

---
+++ SearchingSortingGreedyAlgorithms/BinarySearch/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace BinarySearch
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] numbers = Console.ReadLine()
+				.Split()
+				.Select(int.Parse)
+				.ToArray();
+
+			int number = int.Parse(Console.ReadLine());
+
+			Console.WriteLine(BinarySearch(number, numbers););
+		}
+
+		private static int BinarySearch(int number, int[] numbers)
+		{
+			int left = 0;
+			int right = numbers.Length - 1;
+
+			while (left <= right)
+			{
+				int mid = (left + right) / 2;
+
+				if (numbers[mid] == number)
+					return mid;
+
+				if (number > numbers[mid])
+					left = mid + 1;
+				else
+					right = mid - 1;
+			}
+
+			return -1;
+		}
+	}
+}
